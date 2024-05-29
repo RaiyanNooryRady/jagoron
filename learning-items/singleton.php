683fd7bb39bff69b43817f754f6fd3cf3095f9ae
raiyannooryrady <raiyannooryrady@gmail.com>
2024-05-29 16:55:27 +0600

    singleton

--- learning-items/singleton.php
+++ learning-items/singleton.php
@@ -1,33 +1,48 @@
 <?php
 
-class TraditionalUser{
+class TraditionalUser
+{
     public $user;
-    public function __construct($user){
+    public function __construct($user)
+    {
         $this->user = $user;
     }
 }
-$user1=new TraditionalUser("User1");
-$user2=new TraditionalUser("User2");
+$user1 = new TraditionalUser("User1");
+$user2 = new TraditionalUser("User2");
 echo "In Traditional User class: user1 == user2 >> ";
-echo var_export($user1==$user2)."<br/>";
+echo var_export($user1 == $user2) . "<br/>";
 
-class SingletonUser{
-    private static $instance ;
+class SingletonUser
+{
+    private static $instance;
     private $property;
-    private function __construct($property=null){
-        $this->property= $property;
-        echo $this->property. "<br/>";
+    private function __construct($property = null)
+    {
+        $this->property = $property;
     }
-    public static function getInstance($property=null){
-        if(!isset(self::$instance)){
-         self::$instance = new self($property);
+    public static function getInstance($property = null)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self($property);
         }
         return self::$instance;
     }
+    public function setProperty($property)
+    {
+        $this->property = $property;
+    }
+    public function getProperty()
+    {
+        return $this->property;
+    }
 }
-$user1= SingletonUser::getInstance("user 1");
-$user2= SingletonUser::getInstance("user 2");
-
+$user1 = SingletonUser::getInstance("user 1");
+echo $user1->getProperty()."<br/>"; //output user 1
+$user2 = SingletonUser::getInstance("user 2");
+echo $user2->getProperty()."<br/>"; //output user 1
 
 echo "In singleton User class: user1 == user2 >> ";
-echo var_export($user1==$user2)."<br/>";
+echo var_export($user1 == $user2) . "<br/>";
+$user2->setProperty("User 2"); 
+echo $user2->getProperty(); //output user 2
