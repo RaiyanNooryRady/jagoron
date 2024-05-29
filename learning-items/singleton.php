322e606a005cbf137cd98538a1c9cc1350470e34
raiyannooryrady <raiyannooryrady@gmail.com>
2024-05-29 14:18:00 +0600

    singleton vs traditional class

--- learning-items/singleton.php
+++ learning-items/singleton.php
@@ -1,14 +1,31 @@
 <?php
-class User{
+
+class TraditionalUser{
+    public $user;
+    public function __construct($user){
+        $this->user = $user;
+    }
+}
+$user1=new TraditionalUser("User1");
+$user2=new TraditionalUser("User2");
+echo "In Traditional User class: user1 == user2 >> ";
+echo var_export($user1==$user2)."<br/>";
+
+class SingletonUser{
     private static $instance ;
+    private function __construct(){
+        echo "Private constructor function to prevent direct object creation <br/>";
+    }
     public static function getInstance(){
         if(!isset(self::$instance)){
-         echo 'hello';
          self::$instance = new self();
         }
         return self::$instance;
     }
 }
-$user1= User::getInstance();
-$user2= User::getInstance();
-$user3= User::getInstance();
+$user1= SingletonUser::getInstance();
+$user2= SingletonUser::getInstance();
+$user3= SingletonUser::getInstance();
+
+echo "In singleton User class: user1 == user2 >> ";
+echo var_export($user1==$user2)."<br/>";
