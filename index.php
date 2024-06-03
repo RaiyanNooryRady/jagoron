daa6056d8e8e457002a082291fd983458d800019
raiyannooryrady <raiyannooryrady@gmail.com>
2024-06-03 16:56:47 +0600

    page title added for home page

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
             ?>
             <div class="container">
                 <?php
+                if(is_home()&&!is_front_page()){
+                    ?>
+                    <header class="mb-5">
+                        <h1 class="page-title">
+                            <?php single_post_title(); ?>
+                        </h1>
+                    </header>
+                    <?php
+                }
                 while (have_posts()) {
                     the_post();
                     ?><h2><?php the_title();?></h2><?php
