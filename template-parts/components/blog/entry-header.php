11d9e8d64147b0a350d6332ca5da88185768e249
Noory <raiyannooryrady@gmail.com>
2024-06-08 18:12:09 +0600

    title displayed in blog posts

--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -3,7 +3,9 @@
  * Template for post entry header
  */
 $the_post_id = get_the_ID();
-$hide_title=get_post_meta($the_post_id,"_hide_page_title",true);
+$hide_title = get_post_meta($the_post_id, "_hide_page_title", true);
+$heading_class = !empty($hide_title) && 'yes' === $hide_title ? 'hide' : '';
+
 $has_post_thumbnail = get_the_post_thumbnail($the_post_id);
 ?>
 <header class="entry-header">
@@ -15,15 +17,27 @@ $has_post_thumbnail = get_the_post_thumbnail($the_post_id);
             <a href="<?php echo esc_url(get_permalink()); ?>">
                 <!-- <img src="<?php echo esc_url(get_the_post_thumbnail_url($the_post_id)); ?>" loading="lazy" class="img-fluid"
                     alt=""> -->
-                <?php the_post_custom_thumbnail($the_post_id,'featured-thumbnail',[
-                   'sizes'=>'(max-width:350px)350px,233px',
-                   'class'=>'attachment-featured-large size-featured-image img-fluid'
-               ]); ?>
+                <?php the_post_custom_thumbnail($the_post_id, 'featured-thumbnail', [
+                    'sizes' => '(max-width:350px)350px,233px',
+                    'class' => 'attachment-featured-large size-featured-image img-fluid'
+                ]); ?>
             </a>
 
         </div>
-        <h3><?php the_title(); ?></h3>
         <?php
+    }
+    if (is_single() || is_page()) {
+        printf(
+            '<h1 class="page-title text-dark %1$s">%2$s</h1>',
+            esc_attr($heading_class),
+            wp_kses_post(get_the_title())
+        );
+    } else {
+        printf(
+            '<h2 class="entry-title mb-3"><a href="%1$s" class="link-dark">%2$s</a></h2>',
+            esc_url(get_the_permalink()),
+            wp_kses_post(get_the_title())
+        );
     }
     ?>
 </header>
