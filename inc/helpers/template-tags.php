84aab5803e66154b26afb1dce4ab39ea6393d121
Noory <raiyannooryrady@gmail.com>
2024-06-09 10:04:43 +0600

    posted on information

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -14,6 +14,30 @@ function get_the_post_custom_thumbnail($post_id, $size = 'featured-thumbnail', $
     $custom_thumbnail = wp_get_attachment_image(get_post_thumbnail_id($post_id), $size, false, $additional_attributes);
     return $custom_thumbnail;
 }
-function the_post_custom_thumbnail($post_id, $size = 'featured-image', $additional_attributes = []){
-echo get_the_post_custom_thumbnail($post_id, $size, $additional_attributes);
+function the_post_custom_thumbnail($post_id, $size = 'featured-image', $additional_attributes = [])
+{
+    echo get_the_post_custom_thumbnail($post_id, $size, $additional_attributes);
+}
+function jagoron_posted_on()
+{
+    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+    //if post is modified
+    if (get_the_time('U') != get_the_modified_time('U')) {
+        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+    }
+
+    $time_string = sprintf(
+        $time_string,
+        esc_attr(get_the_date(DATE_W3C)),
+        esc_attr(get_the_date()),
+        esc_attr(get_the_modified_date(DATE_W3C)),
+        esc_attr(get_the_modified_date())
+    );
+
+    $posted_on = sprintf(
+        esc_html_x('Posted on %s', 'post date', 'jagoron'),
+        '<a href="' . esc_url(get_permalink()) . '" rel="bookmark">' . $time_string . '</a>'
+    );
+    echo '<span class="posted-one text-secondary">' . $posted_on . '</span>';
 }
---
+++ template-parts/components/blog/entry-meta.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template for Entry meta
+ * @package  Jagoron
+ */
+?>
+<div class="entry-meta mb-3">
+    <?php jagoron_posted_on(); ?>
+</div>
