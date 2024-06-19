190c406c8734bb2a854fd43e6e15acdc8511554c
DaiyanNDahy <daiyanndahy@gmail.com>
2024-06-19 19:38:20 +0600

    single post page updated

--- single.php
+++ single.php
@@ -1,11 +1,43 @@
 <?php
 /**
- * Main Template File
+ * Single Post Page Template File
  * @package Jagoron
  */
 ?>
 <?php get_header(); ?>
-<div class="content">
-content Single Post Page
+
+<div id="primary">
+    <main id="main" class="site-main mt-5" role="main">
+        <?php
+        if (have_posts()):
+            ?>
+            <div class="container">
+                <?php
+                if (is_home() && !is_front_page()) {
+                    ?>
+                    <header class="mb-5">
+                        <h1 class="page-title">
+                            <?php single_post_title(); ?>
+                        </h1>
+                    </header>
+                    <?php
+                }
+                while (have_posts()):
+                    the_post();
+                    get_template_part("template-parts/content");
+                endwhile;
+                ?>
+
+            </div>
+            <?php
+        else:
+            get_template_part("template-parts/content", "none");
+
+        endif;
+
+
+        ?>
+    </main>
+
 </div>
-<?php get_footer(); 
+<?php get_footer();
