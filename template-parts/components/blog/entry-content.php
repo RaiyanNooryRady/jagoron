92ca6e6d3be7fc76d0538f56e3570ed73c7f9177
DaiyanNDahy <daiyanndahy@gmail.com>
2024-06-19 12:38:34 +0600

    add trim  excerpt to blog

--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -1,1 +1,31 @@
-<div><?php the_excerpt(); ?></div>
+<?php
+/**
+ * Template content
+ * insid wordpress the loop
+ * @package  jagoron
+ */
+?>
+<div class="entry-content">
+    <?php
+    if (is_single()) {
+        the_content(
+            sprintf(
+                wp_kses(
+                    __('continue reading %s <span class="meta-nav">&rarr;</span>', 'jagoron'),
+                    [
+                        'span' => [
+                            'class' => []
+                        ]
+                    ]
+                        ),
+                        the_title('<span class="screen-reader-text">','</span>',false)
+            )
+        );
+    } 
+    else{
+        jagoron_the_excerpt(200);
+
+    }
+    ?>
+
+</div>
