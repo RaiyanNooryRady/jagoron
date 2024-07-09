4c4e83046f61820b1b7334fd03d8cd35ed5dd5bf
DaiyanNDahy <daiyanndahy@gmail.com>
2024-07-09 16:21:05 +0600

    page.php

--- page.php
+++ page.php
@@ -3,9 +3,26 @@
  * Page Template File
  * @package Jagoron
  */
+$the_post_id = get_the_ID();
+$has_post_thumbnail = get_the_post_thumbnail($the_post_id);
 ?>
 <?php get_header(); ?>
 <div class="content">
-content Page
+    <?php if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+            ?>
+            <div class="container">
+                <?php
+                the_title();
+                if ($has_post_thumbnail) {
+                    the_post_thumbnail($the_post_id);
+                }
+                the_content();
+                ?>
+            </div>
+            <?php
+        }
+    } ?>
 </div>
-<?php get_footer(); 
+<?php get_footer();
